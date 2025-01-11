da1448db6a0fe1ad6bd3f12f2da0a22f4916be67
MrMidnight0 <92078595+MrMidnight0@users.noreply.github.com>
2025-01-11 15:55:05 +0200

    Add POST done

--- Controllers/AddController.cs
+++ Controllers/AddController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace LoginPage.Controllers
 {
@@ -72,18 +75,129 @@ namespace LoginPage.Controllers
                     ViewBag.ClientTypes = new SelectList(clientTypes, "ClientTypeId", "ClientTypeName");
                     if (ViewBag.ClientTypes == null)
                     {
-                        return RedirectToAction("Error", new { e = "ClientTypes is null" });
+                        return RedirectToAction("Error", "Client", new { e = "ClientTypes is null" });
                     }
                 }
             }
             catch (Exception ex)
             {
                 // Pass nothing to the view
-                return RedirectToAction("Error", new { e = ex.Message });
+                return RedirectToAction("Error", "Client", new { e = ex.Message });
             }
 
             return View(new ClientAddModel());
         }
-    }
 
+        [HttpPost]
+        public IActionResult AddClient(ClientAddModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMessage"] = "Please ensure all fields are correctly filled in";
+               
+                return RedirectToAction("Add");
+            }
+
+            try
+            {
+                // Convert profile picture to byte array
+                byte[] profilePictureData = null;
+                if (model.ProfilePicture != null)
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        model.ProfilePicture.CopyTo(memoryStream);
+                        profilePictureData = memoryStream.ToArray();
+                    }
+                }
+
+                using (SqlConnection sqlcon = new SqlConnection(conn))
+                {
+                    sqlcon.Open();
+                    //query string for checking if username already exists
+                    string userNameQuery = @"SELECT Count(*) FROM login_details WHERE username = @UserName";
+
+                    using (SqlCommand chkUserName = new SqlCommand(userNameQuery, sqlcon))
+                    {
+                        chkUserName.Parameters.AddWithValue("@UserName", model.Username);
+                        if ((int)chkUserName.ExecuteScalar() > 0)
+                        {
+                            TempData["ErrorMessage"] = "The username already exists.";
+                            
+                            return RedirectToAction("Add");
+                        }
+                    }
+
+                        //query string for adding client
+                        string query = @"INSERT INTO basic_client_information 
+                                            (profile_picture, title_id, client_type_id, name, surname, 
+                                            email, contact_number, address)
+                                        VALUES
+                                            (@ProfilePicture, @TitleId, @ClientType, @Name, 
+                                            @Surname, @Email, @ContactNumber, @Address)
+                                        SELECT SCOPE_IDENTITY()";
+                    //query string for adding client login details
+                    string loginQuery = @"INSERT INTO login_details 
+                                        (client_id, username, password_hash, password_salt)
+                                    VALUES
+                                        (@ClientId, @Username, @PasswordHash, @PasswordSalt)";
+
+                    //adding parameters with model data
+                    using (SqlCommand addCmd = new SqlCommand(query, sqlcon))
+                    {
+                        addCmd.Parameters.AddWithValue("@ProfilePicture", profilePictureData);
+                        addCmd.Parameters.AddWithValue("@TitleId", model.TitleId);
+                        addCmd.Parameters.AddWithValue("@ClientType", model.ClientTypeId);
+                        addCmd.Parameters.AddWithValue("@Name", model.Name);
+                        addCmd.Parameters.AddWithValue("@Surname", model.Surname);
+                        addCmd.Parameters.AddWithValue("@Email", model.Email);
+                        addCmd.Parameters.AddWithValue("@ContactNumber", model.ContactNumber);
+                        addCmd.Parameters.AddWithValue("@Address", model.Address);
+
+                        //get new client id
+                        var newClientID = addCmd.ExecuteScalar();
+                        
+                        //add client in login table
+                        using (SqlCommand loginCmd = new SqlCommand(loginQuery,sqlcon))
+                        {
+                            //generate hashed and salted passwords from username for default 
+                            var passwordSalt = generateSalt(model.Username);
+                            var passwordHash = hashPass(model.Username,passwordSalt);
+
+                            loginCmd.Parameters.AddWithValue("@ClientId", newClientID);
+                            loginCmd.Parameters.AddWithValue("@Username", model.Username);
+                            loginCmd.Parameters.AddWithValue("@PasswordHash", passwordHash);
+                            loginCmd.Parameters.AddWithValue("@PasswordSalt", passwordSalt);
+
+                            loginCmd.ExecuteNonQuery();
+                        }
+                    }
+                    sqlcon.Close();
+
+                    return RedirectToAction("ViewClient", "Client");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Error", "Client", new { e = ex.Message});
+            }
+        }
+
+        //methods for generating salt and hash password
+        private string generateSalt(string username)
+        {
+            var usernameBytes = Encoding.UTF8.GetBytes(username);
+            return Convert.ToBase64String(usernameBytes);
+        }
+        private string hashPass(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var saltedPass = "{password}{salt}";
+                var saltedPassBytes = Encoding.UTF8.GetBytes(saltedPass);
+                var hashBytes = sha256.ComputeHash(saltedPassBytes);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+    }
 }
--- Models/ClientAddModel.cs
+++ Models/ClientAddModel.cs
@@ -2,10 +2,14 @@
 {
     public class ClientAddModel
     {
-        public TitleModel Title { get; set; }
-        public ClientTypeModel ClientType { get; set; }
+        //public TitleModel Title { get; set; }
+        //public ClientTypeModel ClientType { get; set; }
+
+        public string TitleId { get; set; }
+        public string ClientTypeId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public string ContactNumber { get; set; }
         public string Address { get; set; }
