9722684c885332a1358a21d2512275d7115189fd
MrMidnight0 <92078595+MrMidnight0@users.noreply.github.com>
2025-01-11 12:59:54 +0200

    Notes

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,51 @@
+﻿using LoginPage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginPage.Controllers
+{
+    public class NotesController : Controller
+    {
+        public IActionResult Notes()
+        {
+
+            var notes = new List<NotesModel>
+        {
+            new NotesModel
+            {
+                ClientID = 1,
+                NoteID = 1,
+                EmployeeComment = "Suggested a new design layout.",
+                ClientComment = "We like the suggestion!",
+                Date = DateTime.Now.AddDays(-2)
+            },
+            new NotesModel
+            {
+                ClientID = 1,
+                NoteID = 2,               
+                EmployeeComment = "Followed up on the issue.",
+                ClientComment = "Still waiting for resolution.",
+                Date = DateTime.Now.AddDays(-1)
+            },
+            new NotesModel
+            {
+                ClientID = 1,
+                NoteID = 3,                
+                EmployeeComment = "Provided a discount for loyalty.",
+                ClientComment = "Thank you for the discount!",
+                Date = DateTime.Now
+            }
+        };
+
+            return View(notes);
+        }
+        
+        public IActionResult AddEmployeeComment()
+        {
+            return View();
+        }
+        public IActionResult EditComment()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/NotesModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LoginPage.Models
+{
+    public class NotesModel
+    {
+        public int ClientID { get; set; }
+        public int NoteID { get; set; } 
+        public string EmployeeComment { get; set; } 
+        public string ClientComment { get; set; } 
+        public DateTime Date { get; set; } 
+    }
+}
