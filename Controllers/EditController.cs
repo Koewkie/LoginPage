c144b1d60cca30e0018c2a4969a2aea2fb53c5d8
MrMidnight0 <92078595+MrMidnight0@users.noreply.github.com>
2025-01-12 12:18:40 +0200

    edit POST

--- Controllers/EditController.cs
+++ Controllers/EditController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Data.SqlClient;
+using System.Reflection;
 
 namespace LoginPage.Controllers
 {
@@ -10,11 +11,15 @@ namespace LoginPage.Controllers
         string conn = "Data Source=localhost;Initial Catalog=CRM_Test_EstianHuman;Integrated Security=True;";
         // Action to show the Edit Client 
         [HttpGet]
-        public IActionResult Edit()
+        public IActionResult Edit(int ID, ClientEditModel model = null)
         {
             List<TitleModel> titles = new List<TitleModel>();
             List<ClientTypeModel> clientTypes = new List<ClientTypeModel>();
-
+            if (model == null)
+            {
+                model = new ClientEditModel();
+            }
+            //Get titles from DB for viewbag
             try
             {
                 using (SqlConnection sqlcon = new SqlConnection(conn))
@@ -47,7 +52,7 @@ namespace LoginPage.Controllers
                 // Pass nothing to the view
                 return RedirectToAction("Error", new { e = ex.Message });
             }
-
+            //Get ClientTypes from DB for viewbag
             try
             {
                 using (SqlConnection sqlcon = new SqlConnection(conn))
@@ -82,7 +87,189 @@ namespace LoginPage.Controllers
                 return RedirectToAction("Error", new { e = ex.Message });
             }
 
-            return View(new ClientAddModel());
+            //Get existing Client Info from DB for fields
+            try
+            {
+                using (SqlConnection sqlcon = new SqlConnection(conn))
+                {
+                    sqlcon.Open();
+
+                    string query = @"SELECT 
+                                        bci.title_id, bci.client_type_id, bci.name, bci.surname, bci.email, 
+                                        bci.contact_number, bci.address, ld.username 
+                                    FROM 
+                                        basic_client_information AS bci
+                                    INNER JOIN
+                                        login_details AS ld ON bci.client_id = ld.client_id
+                                    WHERE 
+                                        bci.client_id = @ClientId";
+
+                    SqlCommand cmd = new SqlCommand(query, sqlcon);
+                    cmd.Parameters.AddWithValue("@ClientId", ID);
+
+                    SqlDataReader sr = cmd.ExecuteReader();
+
+                    if (sr.Read())
+                    {
+                        model.ClientId = ID;
+                        model.TitleId = sr.GetInt32(0);
+                        model.ClientTypeId = sr.GetInt32(1);
+                        model.Name = sr.GetString(2);
+                        model.Surname = sr.GetString(3);
+                        model.Email = sr.GetString(4);
+                        model.ContactNumber = sr.GetString(5);
+                        model.Address = sr.GetString(6);
+                        model.Username = sr.GetString(7);
+                    }
+                    sr.Close();
+                    sqlcon.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Pass nothing to the view
+                return RedirectToAction("Error", new { e = ex.Message });
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult EditClient(ClientEditModel model)
+        {
+            if (model.ProfilePicture == null)
+            {
+                ModelState.Remove("ProfilePicture");
+            }
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMessage"] = "Please ensure all fields are correctly filled in";
+
+                return RedirectToAction("Edit", model);
+            }
+
+            try
+            {
+                // Convert profile picture to byte array
+                byte[] profilePictureData = null;
+                if (model.ProfilePicture != null)
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        model.ProfilePicture.CopyTo(memoryStream);
+                        profilePictureData = memoryStream.ToArray();
+                    }
+                }
+
+                using (SqlConnection sqlcon = new SqlConnection(conn))
+                {
+                    sqlcon.Open();
+                    //query string for checking if username already exists
+                    string userNameQuery = @"SELECT Count(*) FROM login_details 
+                                             WHERE username = @UserName AND client_id != @ClientId";
+
+                    using (SqlCommand chkUserName = new SqlCommand(userNameQuery, sqlcon))
+                    {
+                        chkUserName.Parameters.AddWithValue("@UserName", model.Username);
+                        chkUserName.Parameters.AddWithValue("@ClientId", model.ClientId);
+
+                        if ((int)chkUserName.ExecuteScalar() > 0)
+                        {
+                            TempData["ErrorMessage"] = "The username already exists.";
+                            var newModel = new ClientAddModel();
+                            newModel.Name = model.Name;
+
+
+                            return RedirectToAction("Edit", model);
+                        }
+                    }
+
+                    if (model.ProfilePicture != null)
+                    {
+                        //query string for updating client (with profile picture)
+                        string queryUpdateClient = @"UPDATE basic_client_information                                            
+                                         SET
+                                            title_id = @TitleId,
+                                            client_type_id = @ClientTypeId,
+                                            name = @Name,
+                                            surname = @Surname,
+                                            email = @Email,
+                                            contact_number = @ContactNumber,
+                                            address = @Address,
+                                            profile_picture = @ProfilePicture
+                                        WHERE
+                                            client_id = @ClientId";
+
+                        //adding parameters with model data
+                        using (SqlCommand updtCmd = new SqlCommand(queryUpdateClient, sqlcon))
+                        {
+                            updtCmd.Parameters.AddWithValue("@ClientId", model.ClientId);
+                            updtCmd.Parameters.AddWithValue("@ProfilePicture", profilePictureData);
+                            updtCmd.Parameters.AddWithValue("@TitleId", model.TitleId);
+                            updtCmd.Parameters.AddWithValue("@ClientTypeId", model.ClientTypeId);
+                            updtCmd.Parameters.AddWithValue("@Name", model.Name);
+                            updtCmd.Parameters.AddWithValue("@Surname", model.Surname);
+                            updtCmd.Parameters.AddWithValue("@Email", model.Email);
+                            updtCmd.Parameters.AddWithValue("@ContactNumber", model.ContactNumber);
+                            updtCmd.Parameters.AddWithValue("@Address", model.Address);
+                            updtCmd.ExecuteNonQuery();
+                        }
+                    }else
+                    {
+                        //query string for updating client (without profilepicture)
+                        string queryUpdateClient = @"UPDATE basic_client_information                                            
+                                         SET
+                                            title_id = @TitleId,
+                                            client_type_id = @ClientTypeId,
+                                            name = @Name,
+                                            surname = @Surname,
+                                            email = @Email,
+                                            contact_number = @ContactNumber,
+                                            address = @Address
+                                        WHERE
+                                            client_id = @ClientId";
+
+                        //adding parameters with model data
+                        using (SqlCommand updtCmd = new SqlCommand(queryUpdateClient, sqlcon))
+                        {
+                            updtCmd.Parameters.AddWithValue("@ClientId", model.ClientId);
+                            updtCmd.Parameters.AddWithValue("@TitleId", model.TitleId);
+                            updtCmd.Parameters.AddWithValue("@ClientTypeId", model.ClientTypeId);
+                            updtCmd.Parameters.AddWithValue("@Name", model.Name);
+                            updtCmd.Parameters.AddWithValue("@Surname", model.Surname);
+                            updtCmd.Parameters.AddWithValue("@Email", model.Email);
+                            updtCmd.Parameters.AddWithValue("@ContactNumber", model.ContactNumber);
+                            updtCmd.Parameters.AddWithValue("@Address", model.Address);
+                            updtCmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    //query to update username in login_details table
+                    string queryUpdateUsername = @"UPDATE login_details                                          
+                                                 SET
+                                                    username = @Username   
+                                                WHERE
+                                                    client_id = @ClientId";
+
+                    //username parameter and execute
+                    using (SqlCommand updtCmd = new SqlCommand(queryUpdateUsername, sqlcon))
+                    {
+                        updtCmd.Parameters.AddWithValue("@Username", model.Username);
+                        updtCmd.Parameters.AddWithValue("@ClientId", model.ClientId);
+
+                        updtCmd.ExecuteNonQuery();
+                    }
+                        
+                    sqlcon.Close();
+
+                    return RedirectToAction("ViewClient", "Client");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Pass nothing to the view
+                return RedirectToAction("Error", "Client", new { e = ex.Message });
+            }
         }
     }
-}
+}           
---
+++ Models/ClientEditModel.cs
@@ -0,0 +1,18 @@
+﻿namespace LoginPage.Models
+{
+    public class ClientEditModel
+    {
+        //public TitleModel Title { get; set; }
+        //public ClientTypeModel ClientType { get; set; }
+        public int ClientId { get; set; }
+        public int TitleId { get; set; }
+        public int ClientTypeId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
+        public string Address { get; set; }
+        public IFormFile ProfilePicture { get; set; }
+    }
+}
